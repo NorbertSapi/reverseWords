23be2873c122a28980640078263a901bc0a087fc
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-22 20:34:12 +0000

    This is the base of the exercise.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # This is an exercise to reverse words in sentence.
+# "Hello World" -> "Word Hello"
+
+test_sentence = "Hello World"
+
+
+def reverse_words(sentence):  # sentence here is an array of characters
+    # TODO: Write - Your - Code
+    return sentence
+
+
+reverse_words(test_sentence)
