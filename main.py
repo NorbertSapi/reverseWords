da56279248e81132a4640194a6c2a1156e22c420
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-23 01:38:18 +0000

    This the algorithm for the exercise.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 # This is an exercise to reverse words in sentence.
 # "Hello World" -> "Word Hello"
 
-test_sentence = "Hello World"
+sentence = "Hello World New Part"
+test_sentence = sentence
 
 
-def reverse_words(sentence):  # sentence here is an array of characters
-    # TODO: Write - Your - Code
-    return sentence
+def cut(test):
+    number = test.index(" ")
+    test = test[(number+1):]
+    return test
 
 
-reverse_words(test_sentence)
+def proceed(test):
+    number = test.index(" ")
+    while number:
+        # number = test.index(" ")
+        if number == test.index(" "):
+            word = test_sentence[0:number]
+            return word
+
+
+# 1st round
+word1 = proceed(test_sentence)
+# 2nd round
+test_sentence = cut(test_sentence)
+word2 = proceed(test_sentence)
+# 3rd round
+test_sentence = cut(test_sentence)
+word3 = proceed(test_sentence)
+# 4th round
+test_sentence = cut(test_sentence)
+
+print("Original:\n\t", sentence)
+print("Test:\n\t", test_sentence, word3, word2, word1)
