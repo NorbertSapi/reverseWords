7526f16b0793a87c7e7941488c6c09dd2d4294ed
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-26 00:13:03 +0000

    This the algorithm for the exercise. Final

--- main.py
+++ main.py
@@ -2,34 +2,36 @@
 # "Hello World" -> "Word Hello"
 
 sentence = "Hello World New Part"
+sentence2 = "This Is A Good Day"
 test_sentence = sentence
+new_sentence = ""
 
 
-def cut(test):
-    number = test.index(" ")
-    test = test[(number+1):]
+# this is a method to return the String back words.
+def cut(test, new):
+    number = 0
+    # it prints the new sentence
+    print(new)
+    # iterate throw the String
+    while number <= len(test):
+        number += 1
+        global new_sentence
+        # condition " ", helps to separate the words in String
+        if number == test.index(" "):
+            # return the substring of the Original String, to build the new String.
+            new_sentence = test[0:number] + " " + new_sentence
+            # substring of the tested sentence. Original String without the first word  of the String.
+            test = test[(number + 1):]
+            # start the sequence again, until no more word in the String.
+            cut(test, new_sentence)
+        elif number != test.index(" "):
+            test = test + " " + new_sentence
     return test
 
 
-def proceed(test):
-    number = test.index(" ")
-    while number:
-        # number = test.index(" ")
-        if number == test.index(" "):
-            word = test_sentence[0:number]
-            return word
-
-
-# 1st round
-word1 = proceed(test_sentence)
-# 2nd round
-test_sentence = cut(test_sentence)
-word2 = proceed(test_sentence)
-# 3rd round
-test_sentence = cut(test_sentence)
-word3 = proceed(test_sentence)
-# 4th round
-test_sentence = cut(test_sentence)
-
-print("Original:\n\t", sentence)
-print("Test:\n\t", test_sentence, word3, word2, word1)
+print("Second test:\n\t")
+word2 = cut(sentence2, new_sentence)
+print(word2)
+print("First test:\n\t")
+word1 = cut(test_sentence, new_sentence)
+print(word1, "\n")
